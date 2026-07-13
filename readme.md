6352bcba3b7ecee9873e7cf1be11c4d9c8c14d06
microcoded <jacob@goodrid.ge>
2026-07-13 13:20:44 +1000

    Initial commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+# remove-marketplaces
+Automatically hides _"marketplace"_ items on various Australian websites.
+
+## Installation Instructions
+- Install Tampermonkey for your browser (or another userscript manager).
+- Click [here](https://github.com/microcoded/remove-marketplaces/raw/main/remove-marketplaces.user.js) and press install.
+
+### Limitations
+This userscript redirects pages to append URL parameters that hide marketplace items. It may not work properly on websites that use SPA routing without doing a full page reload, and parameters will need to be updated if a retailer change their website structure.
---
+++ remove-marketplaces.user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         Remove Marketplaces
+// @author       microcoded (https://github.com/microcoded) & Gemini 3.1 Pro
+// @version      1.0
+// @description  Automatically hides marketplace items on various Australian websites
+// @match        *://*.jbhifi.com.au/*
+// @match        *://*.bunnings.com.au/*
+// @match        *://*.kogan.com/*
+// @match        *://*.dicksmith.com.au/*
+// @match        *://*.woolworths.com.au/*
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const rules = [
+        { domains: ['jbhifi.com.au'], param: 'excludeMarketplace', value: 'true' },
+        { domains: ['bunnings.com.au'], param: 'productranges', value: '!Marketplace' },
+        { domains: ['kogan.com'], param: 'deals_from', value: 'kogan' },
+        { domains: ['dicksmith.com.au'], param: 'deals_from', value: 'dicksmith' },
+        { domains: ['woolworths.com.au'], param: 'filter', value: 'SoldBy(Woolworths)' }
+    ];
+
+    const url = new URL(window.location.href);
+    
+    const activeRule = rules.find(rule => 
+        rule.domains.some(domain => url.hostname.includes(domain))
+    );
+
+    if (activeRule) {
+        const isHomepage = url.pathname === '/' || url.pathname === '';
+        const isCheckout = /\/(cart|checkout)/i.test(url.pathname);
+        
+        if (!isHomepage && !isCheckout) {
+            if (url.searchParams.get(activeRule.param) !== activeRule.value) {
+                url.searchParams.set(activeRule.param, activeRule.value);
+                window.location.replace(url.toString());
+            }
+        }
+    }
+})();
